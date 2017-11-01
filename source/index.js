8d9933a5c0c03a10eb099e13ac5a6c43f981cb8e
Arnelle Balane <arnellebalane@gmail.com>
2017-11-02 01:22:54 +0800

    feat(randomQuotes): accept number of quotes to return
    By default, only one quote is returend. When given an integer parameter, it returns that many number
    of quotes as an array

--- source/index.js
+++ source/index.js
@@ -3,6 +3,14 @@ import quotes from './quotes.json';
 
 const getRandomQuote = uniqueRandomArray(quotes);
 
-export default function randomQuotes() {
-    return getRandomQuote();
+export default function randomQuotes(count=1) {
+    if (count === 1) {
+        return getRandomQuote();
+    }
+
+    const quotesArray = [];
+    for (let i = 0; i < count; i++) {
+        quotesArray.push(getRandomQuote());
+    }
+    return quotesArray;
 }
--- source/index.test.js
+++ source/index.test.js
@@ -2,9 +2,19 @@ import assert from 'assert';
 import randomQuotes from './index';
 
 describe('randomQuotes()', function() {
-    it('should return a quote', function() {
+    it('should return a quote by default', function() {
         const quote = randomQuotes();
         assert(quote.body);
         assert(quote.author);
+    });
+
+    it('should return an array of quotes when passed an integer', function() {
+        const quotes = randomQuotes(3);
+        assert.equal(quotes.length, 3);
+
+        quotes.forEach(quote => {
+            assert(quote.body);
+            assert(quote.author);
+        });
     });
 });
